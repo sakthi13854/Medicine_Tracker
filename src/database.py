d2243c3d391f1ace1c4ece6e38e54a9b67fd1d4f
SAKTHI.S <sakthi138540@gmail.com>
2025-10-20 14:52:36 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,28 +1,29 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+from sqlalchemy.ext.asyncio import AsyncSession,create_async_engine
 
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base\
+from sqlalchemy.ext.declarative import declarative_base
+import ssl
+import os
 
-
-DATABASE_URL = "sqlite+aiosqlite:///./medicine-tracker.db"
-
-engine = create_async_engine(DATABASE_URL, echo=True)
-
-sessionLocal = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False
-
-
-
+Database_Url =os.environ.get("DATABASE_URL")
+if not Database_Url:
+    raise ValueError("DATABASE_URL environment variable not set!")
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False 
+ssl_context.verify_mode = ssl.CERT_NONE
+engine = create_async_engine(
+    Database_Url,
+    echo=True,
+    connect_args={"ssl": ssl_context}
 )
 
-
-
-
+sessionLocal = sessionmaker( bind=engine,
+                             class_=AsyncSession,
+                             expire_on_commit=False)
 Base = declarative_base()
 
-async def get_db():
+async def get_db() -> sessionLocal:
     async with sessionLocal() as session:
         yield session
 
